56f7c479251f79caa8ef9c9851b25032b7f484cb
Noel Bundick <noelbundick@gmail.com>
2015-05-11 23:56:29 -0500

    Add simple chat hub

--- Hubs/SampleHub.cs
+++ Hubs/SampleHub.cs
@@ -1,9 +1,21 @@
+using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 
 namespace API.Hubs
 {
 	public class SampleHub : Hub
 	{
+		public override Task OnConnected() 
+		{
+			Clients.Caller.addMessage("System", "You are now connected");
+			var message = "A new client has connected with id: " + Context.ConnectionId;
+			SendMessage("System", message);
+			return base.OnConnected();
+		}
 		
+		public void SendMessage(string name, string message)
+		{
+			Clients.All.addMessage(name, message);
+		}
 	}
 }
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Routing;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
 
 namespace API
 {
@@ -21,14 +22,25 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSignalR();
             // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
             // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
             // services.AddWebApiConventions();
         }
 
         // Configure is called after ConfigureServices is called.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(LogLevel.Information);
+            
+            //Ghetto CORS support
+            app.Use(async (context, next) => {
+                var origin = context.Request.Headers["Origin"] ?? "http://localhost:9000";
+                context.Response.Headers.Append("Access-Control-Allow-Origin", origin);
+                context.Response.Headers.Append("Access-Control-Allow-Credentials", "true");
+                await next(); 
+            });
+            
             // Configure the HTTP request pipeline.
             app.UseStaticFiles();
 
@@ -36,6 +48,8 @@ namespace API
             app.UseMvc();
             // Add the following route for porting Web API 2 controllers.
             // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            app.UseWebSockets();
+            app.UseSignalR();
         }
     }
 }
